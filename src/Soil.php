d41ead7475391f15cac8d713f9d69f237ec94a44
vanWezel <vanwezeljarno@gmail.com>
2022-03-13 15:51:55 +0100

    hook removed

--- src/Soil.php
+++ src/Soil.php
@@ -7,14 +7,12 @@ class Soil
     public function __construct()
     {
         $this->cleanJsonRoutes();
-        add_action('init', function() {
-            $this->cleanDashboard();
-            $this->cleanEditor();
-            $this->cleanHead();
-            $this->disableComments();
-            $this->setupQueries();
-            $this->setupThemeSupport();
-        });
+        $this->cleanDashboard();
+        $this->cleanEditor();
+        $this->cleanHead();
+        $this->setupQueries();
+        $this->setupThemeSupport();
+        $this->disableComments();
     }
 
     protected function cleanDashboard()
@@ -79,11 +77,7 @@ class Soil
         add_filter('comments_open', '__return_false', 20, 2);
         add_filter('pings_open', '__return_false', 20, 2);
         add_filter('comments_array', '__return_empty_array', 10, 2);
-
-        if (is_admin_bar_showing()) {
-            remove_action('admin_bar_menu', 'wp_admin_bar_comments_menu');
-        }
-
+        remove_action('admin_bar_menu', 'wp_admin_bar_comments_menu');
         add_filter('comments_open', '__return_false', ~PHP_INT_MAX);
 
         foreach (get_post_types() as $type) {
