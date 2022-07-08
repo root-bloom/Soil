011d4df5388c9a32158ddc5ff8dfdeec502cdd7c
Jarno van Wezel <vanwezeljarno@gmail.com>
2022-07-08 09:44:27 +0200

    endpoints disabled

--- src/Soil.php
+++ src/Soil.php
@@ -190,6 +190,14 @@ class Soil
     {
         add_filter('rest_endpoints', function ($endpoints) {
             unset($endpoints['/']);
+
+            if (isset($endpoints['/wp/v2/users'])) {
+                unset($endpoints['/wp/v2/users']);
+            }
+            if (isset($endpoints['/wp/v2/users/(?P<id>[\d]+)'])) {
+                unset($endpoints['/wp/v2/users/(?P<id>[\d]+)']);
+            }
+            
             return $endpoints;
         });
     }
