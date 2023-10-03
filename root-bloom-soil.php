4e698d250e31b6e345dd43d5a2bd41ac15d2c0ae
Jarno van Wezel <jarno@rootandbloom.studio>
2023-10-03 18:02:23 +0200

    New version added

--- root-bloom-soil.php
+++ root-bloom-soil.php
@@ -32,7 +32,6 @@ remove_action('welcome_panel', 'wp_welcome_panel');
 remove_action('template_redirect', 'rest_output_link_header');
 
 // clean up head
-
 remove_action('wp_print_styles', 'print_emoji_styles');
 remove_action('admin_print_styles', 'print_emoji_styles');
 remove_action('admin_print_scripts', 'print_emoji_detection_script');
@@ -54,6 +53,7 @@ remove_action('wp_head', 'wp_shortlink_wp_head');
 remove_action('wp_head', 'wp_oembed_add_discovery_links');
 remove_action('wp_head', 'rest_output_link_wp_head');
 
+// remove yoast helper classname 
 add_action('wp_head', function () {
     ob_start(function ($o) {
         $o = str_replace('class="yoast-schema-graph"', '', $o);
@@ -61,7 +61,23 @@ add_action('wp_head', function () {
     });
 }, ~PHP_INT_MAX);
 
-remove_action('wp_enqueue_scripts', 'wp_enqueue_global_styles');
+// set our logo in the editor instead of wordpress
+add_action('admin_head', function () {
+	echo '<style>
+	.edit-post-header .edit-post-fullscreen-mode-close svg {display:none;}
+	.edit-post-header .edit-post-fullscreen-mode-close::before {
+		background-image: url("/wp-content/mu-plugins/root-bloom-soil/logo.svg");
+		background-repeat: no-repeat;
+		background-position: center center;
+		background-size: contain;
+		box-shadow: none;
+	}
+
+	.editor-styles-wrapper .edit-post-visual-editor__post-title-wrapper { margin: 0 0 2rem 0 !important; padding: 1rem; border-bottom: 1px solid currentColor; }
+	.editor-styles-wrapper h1 { padding: 0.5rem; margin: 0; font-size: 1.5rem; }
+	</style>';
+});
+
 remove_action('wp_body_open', 'wp_global_styles_render_svg_filters');
 add_filter('rss_widget_feed_link', '__return_false');
 
@@ -98,31 +114,19 @@ add_action('admin_init', function () {
     }
 });
 
+// remove comments from admin bar
 add_action('wp_before_admin_bar_render', function () {
     global $wp_admin_bar;
     $wp_admin_bar->remove_menu('comments');
 });
 
-// Remove comments page in menu
+// remove comments page in menu
 add_action('admin_menu', function () {
     remove_menu_page('edit-comments.php');
 });
 
-// modernize team
+// modernize theme
 add_action('after_setup_theme', function () {
-    /**
-     * Disable full-site editing support.
-     *
-     * @link https://wptavern.com/gutenberg-10-5-embeds-pdfs-adds-verse-block-color-options-and-introduces-new-patterns
-     */
-    remove_theme_support('block-templates');
-
-    /**
-     * Disable the default block patterns.
-     * @link https://developer.wordpress.org/block-editor/developers/themes/theme-support/#disabling-the-default-block-patterns
-     */
-    remove_theme_support('core-block-patterns');
-
     /**
      * Enable plugins to manage the document title.
      * @link https://developer.wordpress.org/reference/functions/add_theme_support/#title-tag
@@ -140,25 +144,81 @@ add_action('after_setup_theme', function () {
      * @link https://wordpress.org/gutenberg/handbook/designers-developers/developers/themes/theme-support/#responsive-embedded-content
      */
     add_theme_support('responsive-embeds');
+});
 
-    // todo enable when ready
-    // add_filter('allowed_block_types_all', function ($block_editor_context, $editor_context) {
-    //     if (empty($editor_context->post) || !in_array($editor_context->post->post_type, ['post', 'page'])) {
-    //         return [];
-    //     }
+// parse a block template part via a custom helper
+if (!function_exists('rb_get_block_template')) {
+    function rb_get_block_template($part)
+    {
+        $template_part = get_block_template(get_stylesheet() . '//' . $part, 'wp_template_part');
+        if (!$template_part || empty($template_part->content)) {
+            return;
+        }
 
-    //     return [
-    //         'core/paragraph',
-    //         'core/heading',
-    //         'core/list',
-    //         'core/file',
-    //         'core/audio',
-    //         'core/image',
-    //         'core/video',
-    //         'core/embed',
-    //         'core/columns',
-    //         'core/column',
-    //         'core/text-columns'
-    //     ];
-    // }, 10, 2);
+        return $template_part->content;
+    }
+}
+
+// disable google_gtag js while logged in
+add_action('wp_print_scripts', function () {
+    if (is_user_logged_in()) {
+        wp_dequeue_script('google_gtagjs');
+    }
+}, 100);
+
+// image changes
+add_filter('big_image_size_threshold', function ($threshold) {
+    return 4000;
+}, 999, 1);
+
+// don't resize gifs (so they stay animated)
+add_filter('intermediate_image_sizes_advanced', function ($sizes, $metadata) {
+    $filetype = wp_check_filetype($metadata['file']);
+    if ($filetype['type'] === 'image/gif') {
+        return [];
+    }
+
+    return $sizes;
+}, 10, 2);
+
+// allow svg upload
+add_filter('upload_mimes', function ($mimes) {
+    $mimes['svg'] = 'image/svg+xml';
+    return $mimes;
+});
+
+// set viewport height as root style
+add_action('wp_head', function () {
+    echo '<script type="text/javascript">
+        const root = document.documentElement;
+        root.style.setProperty(\'--rb-vp\', `${window . innerHeight}px`);
+
+        const setSpacingVariables = () => {
+            root.style.setProperty(\'--rb-vp\', `${window . innerHeight}px`);
+        }
+
+        window.addEventListener(\'load\', function() { setSpacingVariables(); });
+        setSpacingVariables();
+    </script>';
+});
+
+// create "backup" while developing for tailwind to compile
+add_action('save_post', function ($post_id) {
+    if (!str_contains(get_home_url(), 'localhost')) {
+        return false;
+    }
+
+    $path = get_theme_file_path('.devcontainer/data/backup');
+    if (!file_exists($path)) {
+        return;
+    }
+
+    $post = get_post($post_id);
+
+    if (str_contains($post->post_name, 'autosave') || str_contains($post->post_name, 'revision') || str_contains($post->post_name, 'trashed')) {
+        return;
+    }
+
+    $path = sprintf('%s/wp-%s.html', $path, $post->post_name);
+    file_put_contents($path, $post->post_content);
 });
